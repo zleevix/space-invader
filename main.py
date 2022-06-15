c4810556b530969c150fc93f48981b0c6d8e886c
Vĩ Lê <lehungvi.cntt@gmail.com>
2022-06-15 21:20:03 +0700

    1 tao cua so tro choi dau tien GAME101x

--- main.py
+++ main.py
@@ -6,23 +6,17 @@ pygame.init()
 
 # create the screen
 screen = pygame.display.set_mode((800,600))
-# Change title
-pygame.display.set_caption("Space Invaders")
-# Change icon pygame
-icon = pygame.image.load("images/ufo.png")
-pygame.display.set_icon(icon)
 
+# Flag check game running: like threading python
 running = True
-
 while running:
     # catch event QUIT
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # without the window open/close immediately
+        if event.type == pygame.QUIT: # Catch when user click X to close pygame window
             running = False
         elif event.type == pygame.KEYDOWN:
             print("KEYDOWN")
-    # RGB 
-    screen.fill((255,0,0))
 
     # Update while running
     pygame.display.update()
